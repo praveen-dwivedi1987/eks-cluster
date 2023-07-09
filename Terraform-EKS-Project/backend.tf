ab560944f3eabf422ccf7d21da45257f9d8d69fa
praveen <praveen.dwivedi65@gmail.com>
2023-07-09 09:56:28 +0530

    fix backend

--- Terraform-EKS-Project/backend.tf
+++ Terraform-EKS-Project/backend.tf
@@ -1,6 +1,6 @@
 #Set S3 backend for persisting TF state file remotely, ensure bucket already exits
 # And that AWS user being used by TF has read/write perms
-
+terraform {
 backend "s3" {
     region  = "us-east-1"
     profile = "default"
